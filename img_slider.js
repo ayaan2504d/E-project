1bbdbdd2c6cc4795b239451cb5180fed53f4cc9d
ayaan2504d <ayaan2504d@aptechsite.net>
2025-11-14 15:54:45 +0500

    ./.

---
+++ img_slider.js
@@ -0,0 +1,22 @@
+  //  let i = 0;
+
+  //   function increment(my_array) {
+  //     if (i < my_array.length) {
+  //       i += 1;
+  //     } else {
+  //       i = 0;
+  //     }
+      
+  //     return i;
+  //   }
+  //   img_array = ["https://ziarre.pk/cdn/shop/files/Luxus-Unisex-Perfume-by-Ziarre-1.jpg?v=1705174491" ,
+  //                "https://perfumedubai.com/cdn/shop/articles/dior_sauvage_460x@2x.jpg?v=1652439946",
+  //                "https://images.unsplash.com/photo-1615634260167-c8cdede054de?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cGVyZnVtZXN8ZW58MHx8MHx8fDA%3D&fm=jpg&q=60&w=3000",
+  //                "https://irfe.com/wp-content/uploads/2025/06/photo-of-a-variety-Different-luxury-perfumes-for-ads-display-in-the-style-of-professioanal-fashion-magazine.jpg",
+  //               "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaDNA7RgJu5Neg1WYC4-TDFAAd1S6UpI5Q5g&s",
+  //               ];
+    
+  //   function changeImg() {
+  //       document.getElementById('image').src = img_array[increment(img_array)];
+  //   }
+    
